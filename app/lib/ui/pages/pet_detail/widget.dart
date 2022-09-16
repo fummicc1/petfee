6f3a621de951e4069d809150cc1d1e36d0f10fb2
Fumiya Tanaka <fumiya.tennis1@gmail.com>
2022-09-16 21:54:22 +0900

    Fix PetDetail (#2)
    * Show indicator when initially loading
    
    * Fix bug

--- app/lib/ui/pages/pet_detail/widget.dart
+++ app/lib/ui/pages/pet_detail/widget.dart
@@ -1,8 +1,10 @@
+import 'package:device_info_plus/device_info_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_calendar_carousel/classes/event.dart';
 import 'package:flutter_calendar_carousel/flutter_calendar_carousel.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:petfee/utils/datetime.dart';
 import '/domain/entities/pet.dart';
 import 'controller.dart';
 import 'state.dart';
@@ -17,64 +19,129 @@ class PetDetailPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final PetDetailState state = ref.watch(petDetailController(pet));
+    var shortestSide = MediaQuery.of(context).size.shortestSide;
+    final bool isTablet = shortestSide >= 600;
     return Scaffold(
       appBar: AppBar(
         title: Text(state.pet.name),
       ),
       body: SingleChildScrollView(
         child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                CalendarCarousel<Event>(
-                  locale: "JA",
-                  dayButtonColor: Theme.of(context).cardColor,
-                  onDayPressed: (selectedDateTime, events) {
-                    ref
-                        .read(petDetailController(pet).notifier)
-                        .updateSelectedDate(selectedDateTime);
-                  },
-                  pageScrollPhysics: const PageScrollPhysics(),
-                  weekdayTextStyle: Theme.of(context).textTheme.bodyText1,
-                  headerTextStyle: Theme.of(context).textTheme.headline6,
-                  weekFormat: false,
-                  height: MediaQuery.of(context).size.height * 0.7,
-                  todayBorderColor: Colors.transparent,
-                  todayButtonColor: Colors.brown,
-                  maxSelectedDate: DateTime.now(),
-                  selectedDayButtonColor: Theme.of(context).colorScheme.secondary,
-                  showOnlyCurrentMonthDate: true,
-                  showHeaderButton: false,
-                  selectedDateTime: state.selectedDate,
-                  selectedDayBorderColor: Colors.transparent,
-                  iconColor: Colors.white,
-                  daysTextStyle: const TextStyle(color: Colors.white),
-                  daysHaveCircularBorder: false,
-                  markedDatesMap: state.calendarEventList,
-                  customGridViewPhysics: const NeverScrollableScrollPhysics(),
-                  markedDateShowIcon: true,
-                  markedDateIconMaxShown: 1,
-                  markedDateIconBuilder: (event) {
-                    return event.icon;
-                  },
-                  markedDateMoreShowTotal: true,
-                ),
-                state.feedsForSelectedDate.isNotEmpty
-                    ? CircularPercentIndicator(
-                        radius: 96,
-                        percent: state.feedCompletionPercentage,
-                        lineWidth: 16,
-                        center: Text(
-                          "${state.feedsForSelectedDate.length}/${state.pet.numberOfFeedTimesPerDay}回達成！",
-                          style: Theme.of(context).textTheme.headline6,
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              CalendarCarousel<Event>(
+                locale: "JA",
+                dayButtonColor: Theme.of(context).cardColor,
+                onDayPressed: (selectedDateTime, events) {
+                  ref
+                      .read(petDetailController(pet).notifier)
+                      .updateSelectedDate(selectedDateTime);
+                },
+                pageScrollPhysics: const PageScrollPhysics(),
+                weekdayTextStyle: Theme.of(context).textTheme.bodyText1,
+                headerTextStyle: Theme.of(context).textTheme.headline6,
+                weekFormat: false,
+                height:
+                    MediaQuery.of(context).size.height * (isTablet ? 0.7 : 0.4),
+                todayBorderColor: Colors.transparent,
+                todayButtonColor: Colors.brown,
+                maxSelectedDate: DateTime.now(),
+                selectedDayButtonColor: Theme.of(context).colorScheme.secondary,
+                showOnlyCurrentMonthDate: true,
+                showHeaderButton: false,
+                selectedDateTime: state.selectedDate,
+                selectedDayBorderColor: Colors.transparent,
+                iconColor: Colors.white,
+                daysTextStyle: const TextStyle(color: Colors.white),
+                daysHaveCircularBorder: false,
+                markedDatesMap: state.calendarEventList,
+                customGridViewPhysics: const NeverScrollableScrollPhysics(),
+                markedDateShowIcon: true,
+                markedDateIconMaxShown: 1,
+                markedDateIconBuilder: (event) {
+                  return event.icon;
+                },
+                markedDateMoreShowTotal: true,
+              ),
+              const SizedBox(height: 24),
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  DefaultTextStyle(
+                    style: Theme.of(context).textTheme.titleMedium!,
+                    child: Row(
+                      children: [
+                        const Text("1日の餌やり回数: "),
+                        Text("${state.pet.numberOfFeedTimesPerDay}回"),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 40),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: DefaultTextStyle(
+                          style: Theme.of(context).textTheme.titleMedium!,
+                          child: const Text("餌やり履歴"),
                         ),
-                      )
-                    : const Center(child: Text("この日の餌の記録が見つかりませんでした")),
-                const SizedBox(
-                  height: 80,
-                ),
-              ],
-            )),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Wrap(
+                          direction: Axis.horizontal,
+                          children: List.generate(
+                              state.pet.numberOfFeedTimesPerDay,
+                              (index) => index + 1).map((i) {
+                            if (i >= state.feedsForSelectedDate.length) {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  padding: const EdgeInsets.all(8),
+                                  decoration: BoxDecoration(
+                                    color:
+                                        Theme.of(context).colorScheme.surface,
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                  child: Text(
+                                      "$i / ${state.pet.numberOfFeedTimesPerDay} 未完了"),
+                                ),
+                              );
+                            } else {
+                              return Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  padding: const EdgeInsets.all(8),
+                                  decoration: BoxDecoration(
+                                    color:
+                                        Theme.of(context).colorScheme.primary,
+                                    borderRadius: BorderRadius.circular(12),
+                                  ),
+                                  child: Row(
+                                    mainAxisSize: MainAxisSize.min,
+                                    children: [
+                                      const Icon(Icons.check),
+                                      Text("$i / ${state.pet.numberOfFeedTimesPerDay} 完了")
+                                    ],
+                                  ),
+                                ),
+                              );
+                            }
+                          }).toList(),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 80),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
       floatingActionButton: Builder(
         builder: (context) => FloatingActionButton.extended(
--- app/lib/utils/datetime.dart
+++ app/lib/utils/datetime.dart
@@ -1,5 +1,12 @@
+import 'package:intl/intl.dart';
+
 extension DateTimeExtension on DateTime {
   DateTime get dropTime {
     return DateTime(year, month, day);
+  }
+
+  String get displayable {
+    DateFormat dateFormat = DateFormat('HH:mm:dd');
+    return dateFormat.format(this);
   }
 }
